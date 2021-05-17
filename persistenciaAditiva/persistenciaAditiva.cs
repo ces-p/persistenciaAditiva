bf5351d5da7b3b860740aa3c6a5a7c44feb139a1
ces-p <cesarportillord@gmail.com>
2021-05-16 20:13:52 -0400

    primer commit

---
+++ persistenciaAditiva/persistenciaAditiva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace persistenciaAditiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 0;
+            int c = x;
+
+            do
+            {
+                while (x / 10 >= 1)
+                {
+                    y += x % 10;
+                    x /= 10;
+
+
+                }
+                Console.WriteLine(y);
+            } while (y > 9);
+
+        }
+        
+    }
+}
