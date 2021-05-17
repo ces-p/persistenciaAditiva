1ca88bd175523890d0b12d25ccd86ef035aef315
JLBelen <jlbelen0900@gmail.com>
2021-05-16 20:45:55 -0400

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace persistenciaAditiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 0;
+            int c = x;
+
+            do
+                
+            {
+                x = c;
+                y = 0;
+                while (x / 10 >= 1)
+                {
+                    y += x % 10;
+                    x /= 10;
+
+                }
+                y += x % 10;
+                Console.WriteLine(y);
+            } while (y > 9);
+
+        }
+        
+    }
+}
